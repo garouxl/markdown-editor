c41d189db6d7ce409c2c13f9c5b47525b8c2c3f5
Leandro Almeida <vempari@gmail.com>
2020-07-23 14:31:33 -0300

    controle inicial de form simples de teste

--- src/app.js
+++ src/app.js
@@ -4,11 +4,35 @@ import React, { Component } from 'react'
 
 import './css/style.css'
 
-const App = () => (
-  <div className='editor'>
-    <textarea />
-    <div className='view' />
-  </div>
-)
+class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      value: ''
+    }
+
+    this.handleSubmit = (e) => {
+      e.preventDefault()
+      this.setState({
+        value: e.target.textarea.value
+      })
+    }
+  }
+
+  render () {
+    return (
+      <div className='editor'>
+        <form onSubmit={this.handleSubmit}>
+          <textarea name='textarea' />
+          <button type='submit'>Renderizar markup</button>
+        </form>
+        <div className='view'>
+          {this.state.value}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default App
