978781e3d617aefd8b48493be8e78384b71ee039
Leandro Almeida <vempari@gmail.com>
2020-07-23 19:41:01 -0300

    Add lib de prop-types e isolado componente markdown

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react'
 
 import './css/style.css'
+import MarkdownEditor from './markdown-editor'
 
 class App extends Component {
   constructor () {
@@ -12,25 +13,19 @@ class App extends Component {
       value: ''
     }
 
-    this.handleSubmit = (e) => {
-      e.preventDefault()
+    this.handleChange = (e) => {
       this.setState({
-        value: e.target.textarea.value
+        value: e.target.value
       })
     }
   }
 
   render () {
     return (
-      <div className='editor'>
-        <form onSubmit={this.handleSubmit}>
-          <textarea name='textarea' />
-          <button type='submit'>Renderizar markup</button>
-        </form>
-        <div className='view'>
-          {this.state.value}
-        </div>
-      </div>
+      <MarkdownEditor
+        value={this.state.value}
+        handleChange={this.handleChange}
+      />
     )
   }
 }
