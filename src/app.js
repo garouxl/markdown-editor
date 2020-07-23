1c86df483782535f855caebb603b73c1d728cec3
Leandro Almeida <vempari@gmail.com>
2020-07-23 11:48:58 -0300

    removido componentes desnecessários e zerado o app.js

--- src/app.js
+++ src/app.js
@@ -4,44 +4,11 @@ import React, { Component } from 'react'
 
 import './css/style.css'
 
-class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      title: '...',
-      Component: 'div'
-    }
-  }
-
-  getTitle () {
-    return new Promise((resolve, reject) => {
-      this.timer = window.setTimeout(() => {
-        resolve('My App with Async / Await!')
-      }, 2000)
-    })
-  }
-
-  async componentDidMount () {
-    try {
-      this.setState({
-        title: await this.getTitle()
-      })
-    } catch (error) {
-      console.log('ERRO:', error)
-    }
-  }
-
-  componentWillMount () {
-    window.clearTimeout(this.timer)
-  }
-
-  render () {
-    return (
-      <div>
-        <this.state.Component>{this.state.title}</this.state.Component>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className='editor'>
+    <textarea />
+    <div className='view' />
+  </div>
+)
 
 export default App
--- src/components/title/title.test.js
+++
@@ -1,9 +0,0 @@
-/* 'use strict'
-
-import { expect } from 'chai'
-import Title from './index'
-
-test('Title should be a function', () => {
-  expect(Title('Teste')).to.be.a('function')
-})
- */
