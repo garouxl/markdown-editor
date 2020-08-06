5d3cca90acb80112d642bc895cbe3e9e40cb6201
Leandro Almeida <vempari@gmail.com>
2020-08-06 12:29:02 -0300

    alterado serviço de deploy

--- src/components/markdown-editor-header.js
+++ src/components/markdown-editor-header.js
@@ -4,7 +4,7 @@ import { PropTypes } from 'prop-types'
 const Header = ({ isSaving }) => (
   <header className='header'>
     <p className={`save-message ${isSaving ? '' : 'saved'}`}>
-      {isSaving ? 'Salvando...' : 'Salvo.'}
+      {isSaving ? 'Salvando...' : 'Salvo'}
     </p>
   </header>
 )
